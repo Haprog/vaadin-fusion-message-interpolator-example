23cec0e25a1c0cf78d030680aaa5937c371a3905
Kari Söderholm <haprog@vaadin.com>
2021-12-22 15:07:09 +0200

    Implement message interpolation
    Use the new interpolateMessageCallback API.
    Needs `@vaadin/form@0.0.16+` or locally link it using `pnpm link`

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,29 @@
+import { Binder } from '@vaadin/form';
 import { Router } from '@vaadin/router';
 import { routes } from './routes';
 import { appStore } from './stores/app-store';
+import { MessageInterpolator } from './localization';
+
+MessageInterpolator.setMessages(new Map<string | RegExp, string>([
+  [
+    '{javax.validation.constraints.Size.message}',
+    'Merkkijonon pituus ei kelpaa. Literal \\{error}.', // escape {error} to show it literally instead of interpolating to undefined
+  ],
+  ['{com.example.my.size.constraint}', 'Merkkijonon pituuden tulee olla vähintään {min} ja enintään {max} merkkiä, oli {size} merkkiä.'],
+  ['{com.example.my.size.constraint.min}', 'Merkkijonon pituuden tulee olla vähintään {min}'],
+  ['must not be blank', 'ei saa olla tyhjä'],
+  ['must not be empty', 'ei saa olla tyhjä'],
+  [
+    /size must be between (?<min>.+) and (?<max>.+)/, // named capturing groups
+    'Merkkijonon pituuden tulee olla väliltä {min} - {max}, oli {size}',
+  ],
+  [
+    /size must be between (.+) and (.+)/, // unnamed capturing groups
+    'Merkkijonon pituuden tulee olla väliltä {1} - {2}, oli {size}',
+  ],
+]));
+
+Binder.interpolateMessageCallback = MessageInterpolator.callback;
 
 export const router = new Router(document.querySelector('#outlet'));
 
