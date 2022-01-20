5d2e3a0732f122d3a8ff3e5b4d25b18f6be58746
Kari Söderholm <haprog@vaadin.com>
2022-01-20 03:32:11 +0200

    Simplify message interpolation logic

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,57 +1,26 @@
-import { Binder, Validator } from '@vaadin/form';
-import * as Validators from '@vaadin/form/Validators';
+import { Binder } from '@vaadin/form';
 import { Router } from '@vaadin/router';
-import { get, registerTranslateConfig, use, lookup } from 'lit-translate';
-import type { Values } from 'lit-translate';
+import { get, registerTranslateConfig, use } from 'lit-translate';
 import { routes } from './routes';
 import { appStore } from './stores/app-store';
 
 // Configure lit-translate
 const translateConfig = registerTranslateConfig({
   loader: lang => fetch(`/i18n/${lang}.json`).then(res => res.json()),
-  lookup: (key, config) => {
-    // First check for root level property including periods/dots in the key
-    if (config.strings && typeof config.strings[key] === 'string') {
-      return config.strings[key] as string;
-    }
-    // Fall back to default implementation
-    return lookup(key, config);
-  }
 });
 use('fi');
 
-// Build a map from the Validators wildcard import
-const validatorToName = new Map<Validator<any>, string>(
-  Object.entries(Validators).map(([id, v]) => [v.prototype, id])
-);
-
-function getValidatorName(validator: Validator<any>) {
-  return validatorToName.get(Object.getPrototypeOf(validator)) || validator.constructor.name;
-}
-
-function getMessagePropertyId(validator: Validator<any>) {
-  return `validationError.${getValidatorName(validator)}`;
-}
-
 Binder.interpolateMessageCallback = (message, validator, binderNode) => {
-  // Use the validator instance properties as values for variable interpolation
-  const values = validator as unknown as Values;
-
-  // Try to find translation for the message (in case of custom message provided from Java annotation)
-  if (translateConfig.lookup(message, translateConfig)) {
-    return get(message, values);
-  }
-
-  let messagePropertyId = getMessagePropertyId(validator);
+  // Try to find a translation for the specific type of validator
+  let key = `validationError.${validator.constructor.name}`;
 
   // Special case for DecimalMin and DecimalMax validators to use different message based on "inclusive" property
-  if (['validationError.DecimalMin', 'validationError.DecimalMax'].includes(messagePropertyId)) {
-    messagePropertyId += (validator as any).inclusive ? '.inclusive' : '.exclusive';
+  if (['validationError.DecimalMin', 'validationError.DecimalMax'].includes(key)) {
+    key += (validator as any).inclusive ? '.inclusive' : '.exclusive';
   }
 
-  // Try to find a default translation for the specific type of validator
-  if (translateConfig.lookup(messagePropertyId, translateConfig)) {
-    return get(messagePropertyId, values);
+  if (translateConfig.lookup(key, translateConfig)) {
+    return get(key, validator as any);
   }
 
   // Fall back to original message if no translations are found
